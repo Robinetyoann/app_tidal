8c31bfd13c9cc0761c072def339e6bf22202837c
Yoann Robinet <yoann@yopbox.com>
2021-10-14 16:05:24 +0200

    Add technos pages

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@
         var router = new Router([
             new Route('home', 'home.html', true),            
             new Route('login', 'login.html'),            
-            new Route('signUp', 'signup.html')
+            new Route('signUp', 'signup.html'),
+            new Route('technos', 'technos.html')
         ]);
     }
     init();
