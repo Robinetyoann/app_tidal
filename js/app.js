3886efae5aa14a8878fe24de2c6bbc573e67c809
Yoann Robinet <yoann@yopbox.com>
2021-10-14 15:18:51 +0200

    init app

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+(function () {
+    function init() {
+        var router = new Router([
+            new Route('home', 'home.html', true),            
+            new Route('login', 'login.html'),            
+            new Route('signUp', 'signup.html')
+        ]);
+    }
+    init();
+}());
